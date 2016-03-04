7b6d87c3b72f9dc35378fc5e1695e1a8b5ba3a9d
Denis Chistyakov (dench) <dench@yandex-team.ru>
2016-03-04 14:29:46 +0500

    1.0.0

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var cssbase = require('borschik/lib/techs/css');
+var csso = require('csso');
+
+exports.Tech = cssbase.Tech.inherit({
+	minimize: function borschikCssoNextMinimize(content) {
+		var opts = this.opts.techOptions.csso || {};
+
+		return csso.minify(content, opts);
+	}
+});
